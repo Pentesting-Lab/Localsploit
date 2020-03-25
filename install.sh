bd12f65d42f61ff153b96a4bd560a7275fa6af40
Pentesting-Lab <62560278+Pentesting-Lab@users.noreply.github.com>
2020-03-25 17:54:41 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get install figlet
+clear
+figlet Localsploit
+echo Installer
+cp localsploit.sh /usr/bin/localsploit.sh
+echo "Localsploit Installed on your computer. Type 'localsploit' in terminal to run it"
